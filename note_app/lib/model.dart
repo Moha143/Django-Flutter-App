3b05e9eeea614659a71436c2ec6a5abbde151691
Mohamed Omar Hassan <54103753+Moha143@users.noreply.github.com>
2022-09-19 15:09:07 +0300

    added notes in the mobile app

---
+++ note_app/lib/model.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+class Note {
+  int id;
+  String note;
+  Note({
+    required this.id,
+    required this.note,
+  });
+
+  Note copyWith({
+    int? id,
+    String? note,
+  }) {
+    return Note(
+      id: id ?? this.id,
+      note: note ?? this.note,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'body': note,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id']?.toInt() ?? 0,
+      note: map['body'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Note.fromJson(String source) => Note.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Note(id: $id, note: $note)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is Note &&
+      other.id == id &&
+      other.note == note;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ note.hashCode;
+}
